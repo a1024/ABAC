8586c488f3d538dcd10fed3cd9f169a0343bc750
a1024 <aymanwmxyz@gmail.com>
2026-02-07 18:12:56 +0200

    wavpsnr fixed

--- psnr/wavpsnr.c
+++ psnr/wavpsnr.c
@@ -1,8 +1,9 @@
+#include<stdint.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 #include<sys/stat.h>
-unsigned char* file_load(const char *srcfn, ptrdiff_t *ret_size)
+uint8_t* file_load(const char *srcfn, ptrdiff_t *ret_size)
 {
 	struct stat info={0};
 	int error=stat(srcfn, &info);
@@ -18,7 +19,7 @@ unsigned char* file_load(const char *srcfn, ptrdiff_t *ret_size)
 		printf("Cannot open \"%s\"", srcfn);
 		return 0;
 	}
-	unsigned char *buf=(unsigned char*)malloc(size+16);
+	uint8_t *buf=(uint8_t*)malloc(size+16);
 	if(!buf)
 	{
 		printf("Alloc error\n");
@@ -34,34 +35,30 @@ unsigned char* file_load(const char *srcfn, ptrdiff_t *ret_size)
 	*ret_size=size;
 	return buf;
 }
-void print_usage(const char *argv0)
-{
-	printf("Usage:  \"%s\"  file1.wav  file2.wav\n", argv0);
-}
 int main(int argc, char **argv)
 {
 	if(argc!=3)
 	{
-		print_usage(argv[0]);
+		printf("Usage:  \"%s\"  file1.wav  file2.wav\n", argv[0]);
 		return 1;
 	}
 	const char *fn1=argv[1], *fn2=argv[2];
 	ptrdiff_t len1=0, len2=0;
-	unsigned short *src1=(unsigned short*)file_load(fn1, &len1);
-	unsigned short *src2=(unsigned short*)file_load(fn2, &len2);
+	uint16_t *src1=(uint16_t*)file_load(fn1, &len1);
+	uint16_t *src2=(uint16_t*)file_load(fn2, &len2);
 	ptrdiff_t len=(len1<len2?len1:len2)/2;
-	long long besterror=0;
+	int64_t besterror=0;
 	int bestshift=0;
 	for(int ko=-256, it=0;ko<1024;++ko, ++it)
 	{
-		unsigned long long error=0;
+		uint64_t error=0;
 		for(int k=0;k<len;++k)
 		{
 			if((unsigned)(k+ko)<len)
 			{
-				int diff=(short)src1[k]-(short)src2[k+ko];
-				error+=(unsigned long long)diff*diff;
-				//if((long long)error<0)//
+				int diff=(int16_t)src1[k]-(int16_t)src2[k+ko];
+				error+=(uint64_t)diff*diff;
+				//if((int64_t)error<0)//
 				//{
 				//	printf("diff %d\n", diff);
 				//	printf("error %lld\n", error);
@@ -79,7 +76,7 @@ int main(int argc, char **argv)
 		}
 	}
 	double rmse=sqrt((double)besterror/len);
-	double psnr=20*log10(0xFFFF/rmse);
+	double psnr=-20*log10(rmse/0x7FFF);
 	printf("RMSE %12.6lf  PSNR %12.6lf  phase shift %d\n", rmse, psnr, bestshift);
 	free(src1);
 	free(src2);
